bb3e1ded426bad53e173b5476a4826bcc2a9135e
RAGHUL-R01 <raghulnishanth195@gmail.com>
2023-12-03 17:25:43 +0530

    SOme more changes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
-const routes = require('./router/routes')
+const cors = require('cors');
+const routes = require('./router/routes');
 const LogData = require('./Middlewares/LogCollector/API_VisitLogger');
-const ErrorLogger = require('./Middlewares/LogCollector/ErrorLogger')
+const ErrorLogger = require('./Middlewares/LogCollector/ErrorLogger');
 
 app.use(bodyParser.json());
-// app.use(LogData)//log details of every API request
+app.use(LogData)//log details of every API request
 
 app.use('/test', (req, res) => {
     res.send("Someone is trying to access the home page");
@@ -17,6 +18,6 @@ app.use('*', (req, res) => {
     res.send("Page not found")
     ErrorLogger("Page not found", req)
 })
-app.listen(4000, () => {
+app.listen(4000, (req,res) => {
     console.log("Running")
 })
--- router/routes.js
+++ router/routes.js
@@ -1,7 +1,7 @@
 const Express = require('express');
 const Route = Express.Router();
-const {emailDetails}=require('./subRoutes/PersonalDetails')
+const { emailDetails } = require('./subRoutes/PersonalDetails')
 
 Route.get('/allMembers', emailDetails)
-console.log("first NAM")
+
 module.exports = Route;
